fbb10e95ba91845fede431a1dd805f7e86cf6626
Jegathish-16 <jegathish162004@gmail.com>
2022-08-30 17:40:46 +0530

    Largest-of-three-numbers

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Sharp
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Three Number: ");
+            int n1, n2, n3;
+            Console.WriteLine("Enter 1st Number: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2st Number: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 3st Number: ");
+            n3 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 > n2 && n1 > n3)
+            {
+                Console.WriteLine("n1 is greater");
+            }
+            else if (n2 > n1 && n2 > n3)
+            {
+                Console.WriteLine("n2 is greater");
+            }
+            else
+            {
+                Console.WriteLine("n3 is greater");
+            }
+        }
+    }
+}
